d91ed3e3390389eb66bef33d05702aa9e60d5d70
y-clps <y.okuyama.clps0723@gmail.com>
2018-08-20 04:24:55 +0900

    section02 exercise

---
+++ introducing_python3/section02/exercise.py
@@ -0,0 +1,6 @@
+print(60*60) #2-1
+seconds_per_hour=60*60 #2-2
+print(seconds_per_hour*24) #2-3
+seconds_per_day=seconds_per_hour*24 #2-4
+print(seconds_per_day/seconds_per_hour) #2-5
+print(seconds_per_day//seconds_per_hour) #2-6
